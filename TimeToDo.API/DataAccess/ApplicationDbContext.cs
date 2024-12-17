a9887b222fe81e83053c8729daf1dabc5edaeb11
ritmh <ritm.homa@gmail.com>
2024-12-17 18:12:27 +0400

    Added models ApplicationUser.cs and configuration

---
+++ TimeToDo.API/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimeToDo.API.DataAccess.Configuration;
+using TimeToDo.Models.Models;
+
+namespace TimeToDo.API.DataAccess;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<ApplicationUser> Users { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new ApplicationUserConfiguration());
+    }
+}
---
+++ TimeToDo.API/DataAccess/Configuration/ApplicationUserConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TimeToDo.Models.Models;
+
+namespace TimeToDo.API.DataAccess.Configuration;
+
+public class ApplicationUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.ToTable("users");
+
+        builder.HasKey(u => u.Id);
+
+        builder.Property(u => u.FullName)
+            .HasColumnType("varchar")
+            .HasMaxLength(256)
+            .IsRequired();
+
+        builder.Property(u => u.UserName)
+            .HasColumnType("varchar")
+            .HasMaxLength(256)
+            .IsRequired();
+
+        builder.HasIndex(u => u.UserName).IsUnique();
+
+        builder.Property(u => u.Email)
+            .HasColumnType("varchar")
+            .HasMaxLength(256)
+            .IsRequired();
+
+        builder.HasIndex(u => u.Email).IsUnique();
+    }
+}
